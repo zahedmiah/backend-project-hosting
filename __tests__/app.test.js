9eaf20453e88008cca456f508d4ddd28570aaf46
Zahed Miah <zahedm70@gmail.com>
2023-02-23 12:06:37 +0000

    attemp on task 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -70,25 +70,49 @@ describe("/api/articles", () => {
         expect(Array.isArray(articles)).toBe(true);
       });
   });
-  test('should return an array of objects', () => {
+  test("should return an array of objects", () => {
     return request(app)
-    .get("/api/articles")
-    .expect(200)
-    .then(({ body }) => {
-      const { articles } = body;
-      articles.forEach((article)=> {
-        expect(article).toEqual(expect.objectContaining( {
-          author: expect.any(String),
-          title: expect.any(String),
-          topic: expect.any(String),
-          created_at: expect.any(String),
-          votes: expect.any(Number),
-          article_img_url: expect.any(String),
-          comment_count: expect.any(Number)
-        })
-        )
-      })
-    })
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(Number),
+            })
+          );
+        });
+      });
+  });
+  describe("GET /api/articles/:articles_id", () => {
+    test("should respond with article objects", () => {
+      return request(app)
+        .get("/api/articles/3")
+        .expect(200)
+        .then(({ body }) => {
+          const { article } = body;
+          expect(article.article_id).toBe(3);
+          expect(article).toEqual(
+            expect.objectContaining({
+              article_id: 3,
+              title: 'Eight pug gifs that remind me of mitch',
+              topic: 'mitch',
+              author: 'icellusedkars',
+              body: 'some gifs',
+              created_at: "2020-11-03T09:12:00.000Z",
+              votes: 0,
+              article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
+              comment_count: '2'
+            })
+          )
+        });
+    });
   });
 });
-
--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,9 +1,24 @@
-const { selectAllArticles } = require("../models/articlesModel");
+const {
+  selectAllArticles,
+  selectArticleByID,
+} = require("../models/articlesModel");
 
 exports.getAllArticles = (req, res, next) => {
   selectAllArticles()
     .then((result) => {
       res.status(200).send({ articles: result });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleByID = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleByID(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+      console.log(article)
     })
     .catch((err) => {
       next(err);
